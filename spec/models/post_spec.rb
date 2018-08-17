989edbf72b38feb3b9fcddbc7fd9cc75da6cc0d0
Marcin Felczak <feleck@gmail.com>
2018-08-17 13:01:40 +0200

    Generate resource for posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  describe 'Creation' do
+    before do
+      @post = Post.create(date: Date.today, rationale: 'Late hours!')
+    end
+    it 'can be created' do
+      expect(@post).to be_valid
+    end
+    it 'cannot be created without a date and rational' do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).not_to be_valid
+    end
+  end
+end
