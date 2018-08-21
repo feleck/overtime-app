1264673f9d17323b54b8f00cbfec1f99a338e444
Marcin Felczak <feleck@gmail.com>
2018-08-21 12:44:56 +0200

    Add full name do user

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Key requirements: documentation of overtime
 
 ## Models
-- Post -> date:date rationale:text
+- x Post -> date:date rationale:text
 - x User -> Devise
 - x AdminUser -> STI
 
@@ -15,8 +15,8 @@
 - Documented - logging
 
 ## UI
-- Bootstrap -> formatting
+- x Bootstrap -> formatting
 
 ## Refactor TODOS:
 - Refactor user association integration test in post_spec
-- Add full name method for users
+- x Add full name method for users
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = User.create(
+      email: 'test@test.com',
+      password: 'asdfasdf',
+      password_confirmation: 'asdfasdf',
+      first_name: 'John',
+      last_name: 'Maklowicz'
+    )
+  end
+
   describe 'creation' do
-    before do
-      @user = User.create(
-        email: 'test@test.com',
-        password: 'asdfasdf',
-        password_confirmation: 'asdfasdf',
-        first_name: 'John',
-        last_name: 'Maklowicz'
-      )
-    end
     it 'can be created' do
       expect(@user).to be_valid
     end
@@ -19,6 +20,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe 'custom name methods' do
+    it 'has a full name method that combines first and last name' do
+      expect(@user.full_name).to eq('MAKLOWICZ, JOHN')
     end
   end
 end
