4750c672923ea7727508c0d8d6d303c286a3eeb2
Marcin Felczak <feleck@gmail.com>
2018-08-22 13:30:01 +0200

    Add new entries link in navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,6 +26,16 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+      click_link('new_post_from_nav')
+      expect(page.status_code).to eq(200)
+
+      # expect(page).to have_content
+    end
+  end
+
   describe 'creation' do
     before do
       visit new_post_path
