32dd25b8bfcd94bc1f5175ffb95d6ef8187977e0
Marcin Felczak <feleck@gmail.com>
2018-09-17 09:07:46 +0200

    Update seeds - users phone numbers

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,16 @@ user = User.create(
   password_confirmation: 'asdfasdf',
   first_name: 'John',
   last_name: 'Maklowicz',
-  phone: '+48732230324'
+  phone: '8732230324'
 )
+
 user2 = User.create(
   email: 'test2@test.com',
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
   first_name: 'Sid',
   last_name: 'Barret',
-  phone: '+48732230324'
+  phone: '8732230324'
 )
 puts '2 users created'
 
@@ -22,7 +23,7 @@ AdminUser.create(
   email: 'admin@user.com',
   password: 'asdfasdf',
   password_confirmation: 'asdfasdf',
-  phone: '+48732230324'
+  phone: '8732230324'
 )
 puts 'admin created'
 
@@ -49,9 +50,9 @@ puts '10 Posts have been created'
 
 10.times do |audit_log|
   AuditLog.create(
-    start_date: (Date.today - 6.days),
     user: user2,
-    status: 0
+    status: 0,
+    start_date: (Date.today - 6.days)
   )
 end
 puts '10 Audit logs have been created!'
