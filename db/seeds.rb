2c155295162ce166ec04ac3c4839e9df77f27c75
Marcin Felczak <feleck@gmail.com>
2018-09-20 10:30:04 +0200

    Add live data to admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,7 @@ puts 'admin created'
 30.times do |post|
   Post.create(
     date: Date.today,
-    rationale: "#{post} rationale content",
+    rationale: "#{post} Kitsch cardigan af wolf stumptown. Irony beard la croix cliche pabst affogato DIY 8-bit kinfolk PBR&B craft beer pop-up butcher put a bird on it disrupt. Vape hexagon williamsburg, authentic flannel cold-pressed wolf. ",
     user: user,
     overtime_request: 2.5
   )
@@ -40,7 +40,7 @@ puts '30 Posts have been created'
 20.times do |post|
   Post.create(
     date: Date.today,
-    rationale: "#{post} rationale content",
+    rationale: "#{post} Freegan polaroid gluten-free enamel pin try-hard fingerstache tousled put a bird on it shoreditch bushwick chia. Umami fashion axe twee etsy tote bag la croix lomo post-ironic microdosing food truck pickled cardigan drinking vinegar coloring book.",
     user: user2,
     overtime_request: 1.4
   )
