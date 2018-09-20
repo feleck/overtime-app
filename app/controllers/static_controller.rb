ce8202479fd96f6de3ef5b8f83443cd4609076db
Marcin Felczak <feleck@gmail.com>
2018-09-20 14:15:42 +0200

    Add view elements for audit items that are pending

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,6 @@
 class StaticController < ApplicationController
   def homepage
     @pending_approvals = Post.where(status: 'submitted')
+    @recent_audit_itmes = AuditLog.last(10) # where(status: 'confirmed')
   end
 end
