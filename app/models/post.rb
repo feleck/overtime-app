59a7dcfc950bfb1e9e6baee10fe0be7c439b52d1
Marcin Felczak <feleck@gmail.com>
2018-09-24 14:30:15 +0200

    Remove self from callback in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ApplicationRecord
   private
 
   def update_audit_log
-    audit_log = AuditLog.where(user_id: self.user_id, start_date: (self.date-7.days..self.date)).last
+    audit_log = AuditLog.where(user_id: user_id, start_date: (date-7.days..date)).last
     audit_log.confirmed! if audit_log
   end
 end
