56601501d0025ccf1389697de5324f5cb7c2f95c
Marcin Felczak <feleck@gmail.com>
2018-09-24 08:45:34 +0200

    Make funcional buttons for employee homepage confirmations

--- app/controllers/audit_logs_controller.rb
+++ app/controllers/audit_logs_controller.rb
@@ -1,7 +1,13 @@
 class AuditLogsController < ApplicationController
-
   def index
     @audit_logs = AuditLog.page(params[:page]).per(10)
     authorize @audit_logs
+  end
+
+  def confirm
+    audit_log = AuditLog.find(params[:id])
+    authorize audit_log
+    audit_log.confirmed!
+    redirect_to root_path, notice: 'Thank you, your confirmation has beem successfully made.'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :audit_logs, except: %i[new edit destroy]
+  resources :audit_logs, except: %i[new edit destroy] do
+    member do
+      get :confirm
+    end
+  end
   namespace :admin do
     resources :posts
     resources :users
