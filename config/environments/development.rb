a7e7ca45e07f7454218db65aab3f3a0b35461d0c
Marcin Felczak <feleck@gmail.com>
2018-09-27 14:46:41 +0200

    Itegrate basic notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,13 +15,14 @@ Rails.application.configure do
   end
 
   config.active_storage.service = :local
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
   config.active_record.verbose_query_logs = true
   config.assets.debug = true
   config.assets.quiet = true
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
