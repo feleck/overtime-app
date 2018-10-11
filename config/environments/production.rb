978c2b5ed7216c6d23ff16074d88ed2fb3179840
Marcin Felczak <feleck@gmail.com>
2018-10-11 15:33:41 +0200

    Add mailer config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,4 +22,5 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'crecer-overtime.herokuapp.com'}
 end
